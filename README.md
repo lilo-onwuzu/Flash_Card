ae71bd0fcfd8b5855ca014fe61df986779142985
Lilo Onwuzu <lpr422@gmail.com>
2016-03-23 14:41:14 -0700

    Fixed all bugs

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# _Calculator_
+# _Flash Card Site_
 
-#### _Business Logic Calculator Project for Epicodus Week 2 Javascript and JQuery_
+#### _Flash Card Site for Javascript and JQuery Tutorial_
 
 #### By _**Ililochi Onwuzu**_
 
 ## Description
 
-_This code prompts the user for two numbers and perfoms an arithmetic operation (addition, subtraction etc) on the numbers. There is a link to the javascript .js file in the html file_
+_This code lists some Javascript and JQuery terms. It contains hidden elements that when clicked on, display more information. Used to practice Javascript at Epicodus Week 2_
 
 ## Setup/Installation Requirements
 
-* _Basic Calculator_
-* _Follow the command prompts_
+* _Click to view description of the listed terms_
+
 
 ## Known Bugs
 
@@ -23,7 +23,7 @@ _lpr422@gmail.com_
 
 ## Technologies Used
 
-_HTML, CSS, Javascript_
+_HTML, CSS, Javascript, JQuery_
 
 ### License
 
--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,32 @@
 //JQuery finds all "p" strings within html and they all become the variable. When you perform a method or event on the variable (click), it implements th function "alert"
 //$ is used to define the function JQuery
 //add or remove class to add new interactive elements (change DOM)e.g add background
-// include comments using (append, prepend, before or after elements)
+//Include comments using (append, prepend, before or after elements)
 ///show/hide/toggle() to show or hide interactive elements
+//toggle allows continous clicking. If you use "show" or "hide" it will show or hide only after the first click but will be unresponsive by the second click
+//The argument ("Argument") callback follows the same name order as in the css page
 
 $(document).ready(function(){
-
-  $("#list").click(function(){
-    $(".marketing").toggle();
-    $("#unlist").toggle();
-    $("#list").toggle();
+  $(".showTerms").click(function(){
+    $(".marketing").show(); //shows the row section when you click.Toggle not needed in this case because the hide term button takes care of the next click
+    $(".hideTerms").show(); //shows the hidden "Click to hide" button
+    $(".showTerms").hide(); //hides the "Click to learn" after you click on it
   });
-
-  $("#unlist").click(function(){
-    $(".marketing").toggle();
-    $("#unlist").toggle();
-    $("#list").toggle();
+  $("p.hideTerms").click(function(){
+    $(".marketing").hide(); //shows & hides the row section when you click
+    $(".hideTerms").hide(); //hides the "Click to Hide" after you click on it
+    $(".showTerms").show(); //displays the "Click to Show" again
   });
-
-  $(".clickable").hover(function(){
-    $("ul").append("p");
+  $(".Javascript .clickable").click(function(){
+    $("#javascript").fadeToggle(); //click on Javascript "clickable" element to display more info in id "javascript" element
   });
-
+  $(".JQuery .clickable").click(function(){
+    $("#jQuery").fadeToggle();
+  });
+  $(".NaN .clickable").click(function(){
+    $("#nan").slideToggle();
+  });
+  $(".Alert .clickable").click(function(){
+    $("#alert").slideToggle();
+  });
 });
